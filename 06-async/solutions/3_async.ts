09bb6d93641b15d4787b8bc2da2c912f2f67071b
Kevin Cunningham <kevin@kevincunningham.co.uk>
2023-10-24 12:43:22 +0100

    bare bones template

--- 06-async/solutions/3_async.ts
+++ 06-async/solutions/3_async.ts
@@ -2,24 +2,39 @@ function getRandomInt(max: number): number {
   return Math.floor(Math.random() * max) + 1;
 }
 
-async function fetchPersonAsync(): Promise<any> {
+interface PersonApiResponse {
+  name: string;
+  homeworld: string;
+}
+
+async function fetchPersonAsync(): Promise<PersonApiResponse> {
   const randomId = getRandomInt(83);
   const response = await fetch(`https://swapi.dev/api/people/${randomId}/`);
   return response.json();
 }
 
-async function fetchPlanetAsync(url: string): Promise<any> {
+interface PlanetApiResponse {
+  name: string;
+}
+
+export async function fetchPlanetAsync(
+  url: string
+): Promise<PlanetApiResponse> {
   const response = await fetch(url);
   return response.json();
 }
 
-(async () => {
+async function run() {
   try {
     const person = await fetchPersonAsync();
-    console.log("Person:", person.name);
+    if (person.name) {
+      console.log("Person:", person.name);
+    }
     const planet = await fetchPlanetAsync(person.homeworld);
     console.log("Homeworld:", planet.name);
   } catch (error) {
     console.error("Error:", error);
   }
-})();
+}
+
+run();
